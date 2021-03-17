5d2ea69af2e761c6284aaed3ce61c4505fe00756
yuziroppe <syani4732@gmail.com>
2021-03-18 02:42:53 +0900

    IOS config以外完成(後は綺麗にまとめる)

--- README.md
+++ README.md
@@ -6,3 +6,7 @@ Automated Firebase Project Setup
 1. install gcloud into your host
 1. setup application default credentials for authentication: `gcloud beta auth application-default login`
 1. export credentials path: `export GOOGLE_APPLICATION_CREDENTIALS="$HOME/.config/gcloud/application_default_credentials.json"`
+
+
+
+Firebase Management API has not been used in project 860919909117 before or it is disabled. Enable it by visiting https://console.developers.google.com/apis/api/firebase.googleapis.com/overview?project=860919909117 then retry. If you enabled this API recently, wait a few minutes for the action to propagate to our systems and retry.
--- nodejs/index.ts
+++ nodejs/index.ts
@@ -1,11 +1,28 @@
 import { google } from 'googleapis';
+import fs from 'fs';
+const firebase = google.firebase('v1beta1');
 const cloudresourcemanager = google.cloudresourcemanager('v1');
 
-async function main() {
-  // TODO: enable json config and parameter options
-  const projectName = 'automated-firebase-test';
-  const projectId = 'automated-firebase-test';
+// TODO: enable json config and parameter options
+const config = {
+  gcp: {
+    projectName: 'automated-firebase-test',
+    projectId: 'automated-firebase-test',
+    firebase: {
+      projectId: 'automated-firebase-test',
+      locationId: 'asia-northeast1',
+      android: {
+        packageName: 'com.yapatta.testapp',
+      },
+      ios: {
+        bundleId: 'com.yapatta.testapp',
+        // appStoreId: ''
+      },
+    },
+  },
+};
 
+async function main() {
   const authClient = await authorize();
 
   const { data, status } = await cloudresourcemanager.projects.list({
@@ -21,34 +38,178 @@ async function main() {
 
   const selectedProjects = data.projects.filter((project) => {
     return (
-      project.projectId === projectId &&
-      project.name === projectName &&
+      // projectId is unique: refer to 'https://cloud.google.com/resource-manager/reference/rest/v1/projects#Project'
+      project.projectId === config.gcp.projectId &&
       project.lifecycleState === 'ACTIVE'
     );
   });
 
+  let projectNumber: string;
   if (selectedProjects.length === 0) {
-    const createResponse = await cloudresourcemanager.projects.create({
+    const createdResponse = await cloudresourcemanager.projects.create({
       auth: authClient,
       requestBody: {
-        projectId: projectId,
-        name: projectName,
+        projectId: config.gcp.projectId,
+        name: config.gcp.projectName,
       },
     });
-    if (createResponse.status !== 200) {
-      throw new Error('bad request: ' + String(createResponse.status));
+    if (createdResponse.status !== 200) {
+      throw new Error('bad request: ' + String(createdResponse.status));
+    }
+
+    const gottenResponse = await cloudresourcemanager.projects.get({
+      projectId: config.gcp.projectId,
+    });
+
+    if (gottenResponse.status !== 200) {
+      throw new Error('bad request: ' + String(createdResponse.status));
+    }
+
+    projectNumber = gottenResponse.data.projectNumber ?? '';
+  } else {
+    projectNumber = selectedProjects.pop()!.projectNumber ?? '';
+  }
+
+  if (projectNumber === '') {
+    throw new Error('unvalidated project number');
+  }
+
+  // create firebase project
+  // TODO: distinguish wheter this project has been already created
+  google.options({ auth: authClient });
+
+  const firebaseProject = await firebase.projects.list();
+
+  // NOTE: the value for PROJECT_IDENTIFIER in any response body will be the ProjectId
+  const selectedFirbaseProjects = firebaseProject.data.results?.filter(
+    (result) => {
+      return (
+        // projectNumber is globally unique: refer to 'https://firebase.google.com/docs/projects/api/reference/rest/v1beta1/projects#FirebaseProject'
+        result.projectNumber === projectNumber && result.state === 'ACTIVE'
+      );
+    },
+  );
+
+  let firebaseName: string;
+  if (
+    selectedFirbaseProjects === undefined ||
+    selectedFirbaseProjects.length === 0
+  ) {
+    const firebaseResponse = await firebase.projects.addFirebase({
+      project: 'projects/' + projectNumber,
+    });
+
+    if (firebaseResponse.status !== 200) {
+      throw new Error('bad request: ' + String(firebaseResponse.status));
+    }
+
+    firebaseName = firebaseResponse.data.name ?? '';
+
+    if (firebaseName === '') {
+      throw new Error('unvalidated firebase name');
+    }
+
+    await firebase.projects.defaultLocation.finalize({
+      parent: firebaseName,
+      requestBody: {
+        locationId: config.gcp.firebase.locationId,
+      },
+    });
+  } else {
+    firebaseName = selectedFirbaseProjects.pop()!.name ?? '';
+    if (firebaseName === '') {
+      throw new Error('unvalidated firebase name');
     }
   }
+
+  const androidAppList = await firebase.projects.androidApps.list({
+    parent: firebaseName,
+  });
+
+  const selectedAndroidAppList = androidAppList.data.apps?.filter((app) => {
+    return app.packageName === config.gcp.firebase.android.packageName;
+  });
+  console.log('android list: ', selectedAndroidAppList);
+
+  if (
+    selectedAndroidAppList === undefined ||
+    selectedAndroidAppList.length === 0
+  ) {
+    const androidAppResponse = await firebase.projects.androidApps.create({
+      parent: firebaseName,
+      requestBody: {
+        packageName: config.gcp.firebase.android.packageName,
+      },
+    });
+
+    if (androidAppResponse.status !== 200) {
+      throw new Error('bad request: ' + String(androidAppResponse.status));
+    }
+
+    console.log('android res:', androidAppResponse);
+  }
+
+  const iosAppList = await firebase.projects.iosApps.list({
+    parent: firebaseName,
+  });
+
+  const selectedIosAppList = iosAppList.data.apps?.filter((app) => {
+    return app.bundleId === config.gcp.firebase.ios.bundleId;
+  });
+  console.log('ios list: ', selectedIosAppList);
+
+  if (selectedIosAppList === undefined || selectedIosAppList.length === 0) {
+    const iosAppResponse = await firebase.projects.iosApps.create({
+      parent: firebaseName,
+      requestBody: {
+        bundleId: config.gcp.firebase.ios.bundleId,
+      },
+    });
+
+    if (iosAppResponse.status !== 200) {
+      throw new Error('bad request: ' + String(iosAppResponse.status));
+    }
+
+    console.log('ios res: ', iosAppResponse);
+  }
+
+  // get android list
+  const l = await firebase.projects.androidApps.list({
+    parent: firebaseName,
+  });
+  const tl = l.data.apps?.find((app) => {
+    return app.packageName === config.gcp.firebase.android.packageName;
+  });
+
+  if (tl === undefined) {
+    throw new Error('not found');
+  }
+
+  const conf = await firebase.projects.androidApps.getConfig({
+    name: tl.name! + '/config',
+  });
+  console.log(conf);
+  fs.writeFile(
+    conf.data.configFilename!,
+    Buffer.from(conf.data.configFileContents!, 'base64'),
+    // eslint-disable-next-line @typescript-eslint/no-empty-function
+    function (err) {},
+  );
 }
 
 async function authorize() {
   const auth = new google.auth.GoogleAuth({
-    scopes: ['https://www.googleapis.com/auth/cloud-platform'],
+    scopes: [
+      'https://www.googleapis.com/auth/cloud-platform',
+      'https://www.googleapis.com/auth/cloud-platform.read-only',
+      'https://www.googleapis.com/auth/firebase',
+      'https://www.googleapis.com/auth/firebase.readonly',
+    ],
   });
 
   return await auth.getClient();
 }
 
 main().catch((err: Error) => {
-  console.error('Message: ', err.message);
+  console.error('message: ', err.message);
 });
